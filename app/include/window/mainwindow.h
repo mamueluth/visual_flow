1fe3f4229dbb14d3221028985e5fb268da33dd17
Manuel <mamueluth@gmail.com>
2022-11-20 16:40:48 +0100

    crude video captureing

--- app/include/window/mainwindow.h
+++ app/include/window/mainwindow.h
@@ -1,7 +1,13 @@
 #ifndef VISUALFLOW_H
 #define VISUALFLOW_H
 
+#include <memory>
+
+#include <QGraphicsPixmapItem>
 #include <QMainWindow>
+#include <QPixmap>
+
+#include <opencv2/opencv.hpp>
 
 QT_BEGIN_NAMESPACE
 namespace Ui
@@ -18,7 +24,23 @@ public:
   VisualFlow(QWidget * parent = nullptr);
   ~VisualFlow();
 
+private slots:
+  void on_startPushButton_clicked();
+  void on_stopPushButton_clicked();
+
+protected:
+  void closeEvent(QCloseEvent * event);
+
 private:
-  Ui::VisualFlow * ui;
+  void display_video_stopped_text();
+
+  std::shared_ptr<QGraphicsScene> scene_;
+  std::shared_ptr<QGraphicsPixmapItem> qt_pixmap_;
+
+  cv::Mat cv_image_;
+  cv::VideoCapture cap_;
+  bool capture_ = false;
+
+  Ui::VisualFlow * ui_;
 };
 #endif  // VISUALFLOW_H
--- app/src/window/mainwindow.cpp
+++ app/src/window/mainwindow.cpp
@@ -1,9 +1,55 @@
 #include "app/include/window/mainwindow.h"
 #include "../../window/ui_mainwindow.h"
 
-VisualFlow::VisualFlow(QWidget * parent) : QMainWindow(parent), ui(new Ui::VisualFlow)
+#include <iostream>
+#include <stdexcept>
+
+#include <QImage>
+
+VisualFlow::VisualFlow(QWidget * parent)
+: QMainWindow(parent), ui_(new Ui::VisualFlow), scene_(std::make_shared<QGraphicsScene>())
 {
-  ui->setupUi(this);
+  ui_->setupUi(this);
+  display_video_stopped_text();
 }
 
-VisualFlow::~VisualFlow() { delete ui; }
+VisualFlow::~VisualFlow() { delete ui_; }
+
+void VisualFlow::on_startPushButton_clicked()
+{
+  cap_ = cv::VideoCapture(0);
+
+  if (!cap_.isOpened())
+  {
+    throw std::runtime_error("Could not open capture device.");
+  }
+
+  capture_ = true;
+  QGraphicsPixmapItem * item;
+  while (capture_ && cap_.isOpened())
+  {
+    cap_ >> cv_image_;
+    if (!cv_image_.empty())
+    {
+      item = new QGraphicsPixmapItem(QPixmap::fromImage(QImage(
+        (unsigned char *)cv_image_.data, cv_image_.cols, cv_image_.rows, QImage::Format_RGB888)));
+      scene_->addItem(item);
+      ui_->graphicsView->setScene(scene_.get());
+      ui_->graphicsView->show();
+    }
+    qApp->processEvents();
+  }
+  cap_.release();
+  display_video_stopped_text();
+}
+
+void VisualFlow::display_video_stopped_text()
+{
+  scene_->addText("Video stopped.");
+  ui_->graphicsView->setScene(scene_.get());
+  ui_->graphicsView->show();
+}
+
+void VisualFlow::on_stopPushButton_clicked() { capture_ = false; }
+
+void VisualFlow::closeEvent(QCloseEvent * event) { capture_ = false; }
